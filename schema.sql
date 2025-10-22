a4c487669ff8423ccd0310378a4367bb2c5fef7a
Hani201623 <152328834+Hani201623@users.noreply.github.com>
2025-10-21 23:56:16 -0400

    Delete schema.sql

--- schema.sql
+++
@@ -1,6 +0,0 @@
--- schema.sql - create demo table
-CREATE TABLE IF NOT EXISTS products (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  price DECIMAL(10,2) NOT NULL DEFAULT 0.00
-);
